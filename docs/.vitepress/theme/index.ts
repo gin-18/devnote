eecf5d41462a77bbcfe16d2017d3140768d74da2
gin <gin-18@qq.com>
2026-01-10 16:08:13 +0800

    应用catppuccin-blue主题

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,7 +2,8 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import './style.css'
+import "@catppuccin/vitepress/theme/macchiato/blue.css"
+// import './style.css'
 
 export default {
   extends: DefaultTheme,
