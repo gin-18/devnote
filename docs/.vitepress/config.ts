980d032f1c5da1028f38fa3e8a3d82f3730863dc
gin <gin-18@qq.com>
2026-01-14 11:05:13 +0800

    style: 格式化config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "DevNote",
-  description: "notes about study and work",
+  title: 'DevNote',
+  description: 'notes about study and work',
 
   lang: 'zh-CN',
   locales: {
     '/': {
       label: '简体中文',
-      lang: 'zh-CN'
-    }
+      lang: 'zh-CN',
+    },
   },
 
   themeConfig: {
@@ -19,20 +19,21 @@ export default defineConfig({
 
     docFooter: {
       prev: '上一篇',
-      next: '下一篇'
+      next: '下一篇',
     },
 
     outlineTitle: '页面内容',
 
     nav: [
       { text: '学习笔记', link: '/study-note/git/git-stage/git-stage' },
-      { text: '工作笔记', link: '/work-note/rebue-component/rebue-form/rebue-form' }
+      { text: '工作笔记', link: '/work-note/rebue-component/rebue-form/rebue-form' },
     ],
 
     sidebar: {
       '/study-note/': [
         {
           text: 'Git',
+          collapsed: true,
           items: [
             { text: 'git 暂存', link: '/study-note/git/git-stage/git-stage' },
             { text: 'git 贮藏', link: '/study-note/git/git-stash/git-stash' },
@@ -40,51 +41,47 @@ export default defineConfig({
             { text: 'git 冲突的解决', link: '/study-note/git/git-conflict-resolve/git-conflict-resolve' },
             { text: 'git 标签', link: '/study-note/git/git-tag/git-tag' },
             { text: 'git 远程仓库', link: '/study-note/git/git-remote/git-remote' },
-            { text: 'git 的配置项', link: '/study-note/git/git-config/git-config' }
-          ]
+            { text: 'git 的配置项', link: '/study-note/git/git-config/git-config' },
+          ],
         },
         {
           text: 'Windows',
+          collapsed: true,
           items: [
             { text: 'windows 终端的配置', link: '/study-note/windows/windows-terminal/windows-terminal' },
             { text: 'powershell 的配置', link: '/study-note/windows/powershell/powershell' },
             { text: 'winget 基本使用', link: '/study-note/windows/winget/winget' },
-            { text: 'wsl 基本使用', link: '/study-note/windows/wsl/wsl' }
-          ]
+            { text: 'wsl 基本使用', link: '/study-note/windows/wsl/wsl' },
+          ],
         },
         {
           text: 'Coding',
-          items: [
-            { text: 'todo comment 代码标签', link: '/study-note/coding/todo-comment/todo-comment' },
-          ]
+          collapsed: true,
+          items: [{ text: 'todo comment 代码标签', link: '/study-note/coding/todo-comment/todo-comment' }],
         },
         {
           text: 'Markdown',
-          items: [
-            { text: 'markdown 基本语法', link: '/study-note/markdown/markdown' },
-          ]
+          collapsed: true,
+          items: [{ text: 'markdown 基本语法', link: '/study-note/markdown/markdown' }],
         },
         {
           text: 'SSH',
-          items: [
-            { text: 'ssh 基本使用', link: '/study-note/ssh/ssh' },
-          ]
+          collapsed: true,
+          items: [{ text: 'ssh 基本使用', link: '/study-note/ssh/ssh' }],
         },
         {
           text: 'v2ray',
-          items: [
-            { text: '搭建代理服务器', link: '/study-note/v2ray/v2ray' },
-          ]
-        }
+          collapsed: true,
+          items: [{ text: '搭建代理服务器', link: '/study-note/v2ray/v2ray' }],
+        },
       ],
       '/work-note/': [
         {
           text: 'Rebue Component',
-          items: [
-            { text: 'RebueForm', link: '/work-note/rebue-component/rebue-form/rebue-form' }
-          ]
-        }
+          collapsed: true,
+          items: [{ text: 'RebueForm', link: '/work-note/rebue-component/rebue-form/rebue-form' }],
+        },
       ],
-    }
-  }
+    },
+  },
 })
