cf6bf00e2d31dd12dc465941b6fd34dd3f34adeb
gin <gin-18@qq.com>
2026-03-10 20:20:58 +0800

    修改git的菜单顺序

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -40,8 +40,8 @@ export default defineConfig({
             { text: 'git 标签', link: '/study-note/git/git-tag/git-tag' },
             { text: 'git 远程仓库', link: '/study-note/git/git-remote/git-remote' },
             { text: 'git 的配置项', link: '/study-note/git/git-config/git-config' },
-            { text: 'conventional-commits 提交规范', link: '/study-note/git/conventional-commits/conventional-commits' },
             { text: '服务器上的 git', link: '/study-note/git/server-git/server-git' },
+            { text: 'conventional-commits 提交规范', link: '/study-note/git/conventional-commits/conventional-commits' },
           ],
         },
         {
